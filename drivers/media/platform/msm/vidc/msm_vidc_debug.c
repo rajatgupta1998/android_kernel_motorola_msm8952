6cacf4c32e3ef9177a7163cc7fc0084638aaa8d4
Abdulla Anam <abdullahanam@codeaurora.org>
2017-03-22 14:00:10 +0530

    msm: vidc: Protect debug_buffer access in info_read with lock
    Serialize core_info_read & inst_info_read with lock so that
    multiple concurrent threads do not cause the write to
    overflow. Also have the bound check to avoid overflow in
    write_str function..
    
    CRs-fixed: 2013361
    Mot-CRs-fixed: (CR)
    
    Change-Id: Ie63f74e592319c2864e206b3d242733a8a25bacd
    Signed-off-by: Abdulla Anam <abdullahanam@codeaurora.org>
    Signed-off-by: Srikanth A R <arsrikan@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1002914
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    
    Conflicts:
    	drivers/media/platform/msm/vidc/msm_v4l2_vidc.c
    	drivers/media/platform/msm/vidc/msm_vidc_debug.c

--- drivers/media/platform/msm/vidc/msm_vidc_debug.c
+++ drivers/media/platform/msm/vidc/msm_vidc_debug.c
@@ -32,6 +32,20 @@ int msm_vidc_sys_idle_indicator = 0x0;
 u32 msm_vidc_firmware_unload_delay = 15000;
 int msm_vidc_thermal_mitigation_disabled = 0x0;
 
+struct debug_buffer {
+	struct mutex lock;
+	char ptr[MAX_DBG_BUF_SIZE];
+	char *curr;
+	u32 filled_size;
+};
+
+static struct debug_buffer dbg_buf;
+
+#define INIT_DBG_BUF(__buf) ({ \
+	__buf.curr = __buf.ptr;\
+	__buf.filled_size = 0; \
+})
+
 #define DYNAMIC_BUF_OWNER(__binfo) ({ \
 	atomic_read(&__binfo->ref_count) == 2 ? "video driver" : "firmware";\
 })
@@ -51,10 +65,13 @@ static u32 write_str(char *buffer,
 		size_t size, const char *fmt, ...)
 {
 	va_list args;
-	u32 len;
+	u32 size;
+
+	char *curr = buffer->curr;
+	char *end = buffer->ptr + MAX_DBG_BUF_SIZE;
 
 	va_start(args, fmt);
-	len = vscnprintf(buffer, size, fmt, args);
+	size = vscnprintf(curr, end - curr, fmt, args);
 	va_end(args);
 	return len;
 }
@@ -83,10 +100,12 @@ static ssize_t core_info_read(struct file *file, char __user *buf,
 	end = cur + MAX_DBG_BUF_SIZE;
 	hdev = core->device;
 
-	cur += write_str(cur, end - cur, "===============================\n");
-	cur += write_str(cur, end - cur, "CORE %d: %pK\n", core->id, core);
-	cur += write_str(cur, end - cur, "===============================\n");
-	cur += write_str(cur, end - cur, "Core state: %d\n", core->state);
+	mutex_lock(&dbg_buf.lock);
+	INIT_DBG_BUF(dbg_buf);
+	write_str(&dbg_buf, "===============================\n");
+	write_str(&dbg_buf, "CORE %d: 0x%pK\n", core->id, core);
+	write_str(&dbg_buf, "===============================\n");
+	write_str(&dbg_buf, "Core state: %d\n", core->state);
 	rc = call_hfi_op(hdev, get_fw_info, hdev->hfi_device_data, &fw_info);
 	if (rc) {
 		dprintk(VIDC_WARN, "Failed to read FW info\n");
@@ -110,9 +129,9 @@ err_fw_info:
 			"pending" : "done");
 	}
 	len = simple_read_from_buffer(buf, count, ppos,
-			dbuf, cur - dbuf);
+			dbg_buf.ptr, dbg_buf.filled_size);
 
-	kfree(dbuf);
+	mutex_unlock(&dbg_buf.lock);
 	return len;
 }
 
@@ -168,6 +187,7 @@ struct dentry *msm_vidc_debugfs_init_drv(void)
 {
 	struct dentry *dir = NULL;
 
+	mutex_init(&dbg_buf.lock);
 	dir = debugfs_create_dir("msm_vidc", NULL);
 	if (IS_ERR_OR_NULL(dir)) {
 		dir = NULL;
@@ -338,39 +358,15 @@ static ssize_t inst_info_read(struct file *file, char __user *buf,
 	int i, j;
 	ssize_t len = 0;
 
-	if (!idata || !idata->core || !idata->inst) {
-		dprintk(VIDC_ERR, "%s: Invalid params\n", __func__);
-		return 0;
-	}
-
-	core = idata->core;
-	inst = idata->inst;
-
-	mutex_lock(&core->lock);
-	list_for_each_entry(temp, &core->instances, list) {
-		if (temp == inst)
-			break;
-	}
-	inst = (temp == inst)?
-		inst : NULL;
-	mutex_unlock(&core->lock);
-
 	if (!inst) {
 		dprintk(VIDC_ERR, "%s: Instance has become obsolete", __func__);
 		return 0;
 	}
 
-	dbuf = kzalloc(MAX_DBG_BUF_SIZE, GFP_KERNEL);
-	if (!dbuf) {
-		dprintk(VIDC_ERR, "%s: Allocation failed!\n", __func__);
-		len = -ENOMEM;
-		goto failed_alloc;
-	}
-	cur = dbuf;
-	end = cur + MAX_DBG_BUF_SIZE;
-
-	cur += write_str(cur, end - cur, "==============================\n");
-	cur += write_str(cur, end - cur, "INSTANCE: %pK (%s)\n", inst,
+	mutex_lock(&dbg_buf.lock);
+	INIT_DBG_BUF(dbg_buf);
+	write_str(&dbg_buf, "===============================\n");
+	write_str(&dbg_buf, "INSTANCE: 0x%pK (%s)\n", inst,
 		inst->session_type == MSM_VIDC_ENCODER ? "Encoder" : "Decoder");
 	cur += write_str(cur, end - cur, "==============================\n");
 	cur += write_str(cur, end - cur, "core: %pK\n", inst->core);
@@ -439,16 +435,10 @@ static ssize_t inst_info_read(struct file *file, char __user *buf,
 	len = simple_read_from_buffer(buf, count, ppos,
 		dbuf, cur - dbuf);
 
-	kfree(dbuf);
-failed_alloc:
+	len = simple_read_from_buffer(buf, count, ppos,
+		dbg_buf.ptr, dbg_buf.filled_size);
+	mutex_unlock(&dbg_buf.lock);
 	return len;
-}
-
-static int inst_info_release(struct inode *inode, struct file *file)
-{
-	dprintk(VIDC_INFO, "Release inode ptr: %pK\n", inode->i_private);
-	file->private_data = NULL;
-	return 0;
 }
 
 static const struct file_operations inst_info_fops = {
@@ -570,5 +560,10 @@ void msm_vidc_debugfs_update(struct msm_vidc_inst *inst,
 		dprintk(VIDC_ERR, "Invalid state in debugfs: %d\n", e);
 		break;
 	}
+}
+
+void msm_vidc_debugfs_deinit_drv(void)
+{
+	mutex_destroy(&dbg_buf.lock);
 }
 
