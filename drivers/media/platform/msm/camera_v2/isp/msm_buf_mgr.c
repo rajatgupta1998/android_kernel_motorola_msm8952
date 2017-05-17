b0626691642407e3569867689999bcc0cb7b6065
Gaoxiang Chen <gaochen@codeaurora.org>
2017-05-17 15:14:36 +0800

    msm: camera: fix off-by-one overflow in msm_isp_get_bufq
    In msm_isp_get_bufq, if bufq_index equals buf_mgr->num_buf_q,
    it will pass the check, leading to off-by-one overflow
    (exceed the length of array by one element).
    
    CRs-Fixed: 2031677
    Bug: 36136563
    CVE-fixed: CVE-2017-11000
    Mot-CRs-fixed: (CR)
    
    Change-Id: I7ea465897e2c37de6ca0155c3e225f1444b3cf13
    Signed-off-by: Gaoxiang Chen <gaochen@codeaurora.org>
    Signed-off-by: Srikanth A R <arsrikan@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1043223
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key
    (cherry picked from commit 12a3c12655b2ed83a28b31a6b87a5681ca1e39a2)
    
    Conflicts:
    	drivers/media/platform/msm/camera_v2/isp/msm_buf_mgr.c

--- drivers/media/platform/msm/camera_v2/isp/msm_buf_mgr.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_buf_mgr.c
@@ -91,7 +91,7 @@ struct msm_isp_bufq *msm_isp_get_bufq(
 
 	if ((bufq_handle == 0) ||
 		(bufq_index >= buf_mgr->num_buf_q) ||
-		(bufq_index >= BUF_MGR_NUM_BUF_Q))
+		(bufq_index >= BUF_MGR_NUM_BUF_Q) )
 		return NULL;
 
 	bufq = &buf_mgr->bufq[bufq_index];
