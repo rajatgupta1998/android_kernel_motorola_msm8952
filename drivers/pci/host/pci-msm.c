e72e19522e7bc65fb22f4563980f41ff835596a0
Sultan Alsawaf <sultanxda@gmail.com>
2017-05-31 15:39:11 -0700

    msm: pcie: Fix uninitialized pointer usage in msm_pcie_debug_info()
    pdev is used uninitialized in the PCIE_DBG_FS() invocations above its
    initialization. Fix it.
    
    Change-Id: I7248009a0a15ea0979ede61395406615816a6cb4
    Signed-off-by: Sultan Alsawaf <sultanxda@gmail.com>
    Signed-off-by: Rohit Vashisht <rohitv88cf@gmail.com>

--- drivers/pci/host/pci-msm.c
+++ drivers/pci/host/pci-msm.c
@@ -1755,6 +1755,8 @@ int msm_pcie_debug_info(struct pci_dev *dev, u32 option, u32 base,
 		return -ENODEV;
 	}
 
+	pdev = PCIE_BUS_PRIV_DATA(dev);
+
 	if (option == 12 || option == 13) {
 		if (!base || base > 5) {
 			pr_alert("Invalid base_sel: 0x%x\n", base);
@@ -1776,7 +1778,6 @@ int msm_pcie_debug_info(struct pci_dev *dev, u32 option, u32 base,
 		}
 	}
 
-	pdev = PCIE_BUS_PRIV_DATA(dev);
 	rc_sel = 1 << pdev->rc_idx;
 
 	msm_pcie_sel_debug_testcase(pdev, option);
