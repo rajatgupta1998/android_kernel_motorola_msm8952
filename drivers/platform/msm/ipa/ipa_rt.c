2ccbb9bf2c15dc30d30c1e47c278f21893503de9
Mohammed Javid <mjavid@codeaurora.org>
2017-06-15 15:39:04 +0530

    msm:ipa: Fix to incorrect structure access
    Accessing of incorrect structure pointer is causing
    memory out of bound access, fixed issue by accessing
    the correct structure pointer.
    
    Mot-CRs-fixed: (CR)
    CVE-Fixed: CVE-2017-14879
    CRs-Fixed: 2056307
    Bug: 63890276
    
    Change-Id: I3c2f5f7a97cac854093ef670184d06db4231f5e1
    Acked-by: Ashok Vuyyuru <avuyyuru@qti.qualcomm.com>
    Signed-off-by: Mohammed Javid <mjavid@codeaurora.org>
    Reviewed-on: https://gerrit.mot.com/1106230
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key
    
    Conflicts:
    	drivers/platform/msm/ipa/ipa_rt.c

--- drivers/platform/msm/ipa/ipa_rt.c
+++ drivers/platform/msm/ipa/ipa_rt.c
@@ -1336,6 +1336,10 @@ int ipa_get_rt_tbl(struct ipa_ioc_get_rt_tbl *lookup)
 	mutex_lock(&ipa_ctx->lock);
 	entry = __ipa_find_rt_tbl(lookup->ip, lookup->name);
 	if (entry && entry->cookie == IPA_RT_TBL_COOKIE) {
+		if (entry->ref_cnt == U32_MAX) {
+			IPAERR("fail: ref count crossed limit\n");
+			goto ret;
+		}
 		entry->ref_cnt++;
 		lookup->hdl = entry->id;
 
