6d5e9ea714f624d1dbacfa4e09c5bdf655d83620
kaiwang <kaiwang@codeaurora.org>
2017-06-27 19:29:03 +0800

    msm: camera: sensor:validating the flash initialization parameters
    Copying the flash initialization parameters from userspace memory to
    kernel memory and in turn checking for the validity of the flash
    initialization parameters pointer sent from userspace
    
    CRs-Fixed: 2059812
    CVE-fixed: CVE-2017-11057
    Bug: 37949660
    Mot-CRs-fixed: (CR)
    
    Change-Id: I957c10959108eb08b263d439a9a449b90338b6db
    Signed-off-by: kaiwang <kaiwang@codeaurora.org>
    Signed-off-by: Srikanth A R <arsrikan@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1057742
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key
    (cherry picked from commit f19bf91a2a0438c83839fec78ce4a501b85982fe)
    Signed-off-by: Rajat Gupta <rjtgupta84@gmail.com>
    
    Conflicts:
    	drivers/media/platform/msm/camera_v2/sensor/flash/msm_flash.c

--- drivers/media/platform/msm/camera_v2/sensor/flash/msm_flash.c
+++ drivers/media/platform/msm/camera_v2/sensor/flash/msm_flash.c
@@ -531,7 +531,7 @@ static int32_t msm_flash_init_prepare(
 	struct msm_flash_cfg_data_t flash_data_k;
 	struct msm_flash_init_info_t flash_init_info;
 	int32_t i = 0;
-	if(!is_compat_task()) {
+	if (!is_compat_task()) {
 		/*for 64-bit usecase,it need copy the data to local memory*/
 		flash_data_k.cfg_type = flash_data->cfg_type;
 		for (i = 0; i < MAX_LED_TRIGGERS; i++) {
@@ -540,7 +540,6 @@ static int32_t msm_flash_init_prepare(
 			flash_data_k.flash_duration[i] =
 				flash_data->flash_duration[i];
 		}
-
 		flash_data_k.cfg.flash_init_info = &flash_init_info;
 		if (copy_from_user(&flash_init_info,
 			(void *)(flash_data->cfg.flash_init_info),
@@ -571,13 +570,13 @@ static int32_t msm_flash_init_prepare(
 
 	flash_data_k.cfg.flash_init_info = &flash_init_info;
 	if (copy_from_user(&flash_init_info,
-		(void *)(flash_data->cfg.flash_init_info),
-		sizeof(struct msm_flash_init_info_t))) {
-		pr_err("%s copy_from_user failed %d\n",
-			__func__, __LINE__);
-		return -EFAULT;
-	}
-	return msm_flash_init(flash_ctrl, &flash_data_k);
+			(void *)(flash_data->cfg.flash_init_info),
+			sizeof(struct msm_flash_init_info_t))) {
+			pr_err("%s copy_from_user failed %d\n",
+				__func__, __LINE__);
+			return -EFAULT;
+		}
+return msm_flash_init(flash_ctrl, &flash_data_k);
 #endif
 }
 
