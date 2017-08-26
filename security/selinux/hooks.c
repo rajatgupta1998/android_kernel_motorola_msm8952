f33a8a4bb4ec9e300a4850436e456f096a43fa46
rahulsnair <rahulsnair30@gmail.com>
2017-08-26 18:11:13 +0000

    selinux: Force kernel to boot in permissive
    Change-Id: If150e50ec241cc26f0f3bbf9cc87e88335b11e53

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -110,7 +110,7 @@ static int __init enforcing_setup(char *str)
 {
 	unsigned long enforcing;
 	if (!strict_strtoul(str, 0, &enforcing))
-		selinux_enforcing = enforcing ? 1 : 0;
+		selinux_enforcing = 0; //enforcing ? 1 : 0;
 	return 1;
 }
 __setup("enforcing=", enforcing_setup);
