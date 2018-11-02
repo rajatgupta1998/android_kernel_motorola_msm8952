98b73df25a4bef86fc514d09f99e91542fbf5f1e
Rohit Vashisht <rohitv88cf@gmail.com>
2018-11-02 14:23:47 +0530

    Revert "driver core: fix race between creating/querying glue dir and its cleanup"
    Fixed in linux-stable v3.10.105 upstream.
    
    This reverts commit 8df501e4fe4e66d2eed412613272e966a64537cc.
    
    Signed-off-by: Rohit Vashisht <rohitv88cf@gmail.com>

--- drivers/base/core.c
+++ drivers/base/core.c
@@ -826,35 +826,22 @@ static struct kobject *get_device_parent(struct device *dev,
 		return &parent->kobj;
 	return NULL;
 }
-static inline bool live_in_glue_dir(struct kobject *kobj,
-					struct device *dev)
-{
-	if (!kobj || !dev->class ||
-	    kobj->kset != &dev->class->p->glue_dirs)
-		return false;
-	return true;
-}
 
-static inline struct kobject *get_glue_dir(struct device *dev)
-{
-	if (live_in_glue_dir(&dev->kobj, dev))
-		return dev->kobj.parent;
-	return NULL;
-}
-/*
-* make sure cleaning up dir as the last step, we need to make
-* sure .release handler of kobject is run with holding the
-* global lock
-*/
 static void cleanup_glue_dir(struct device *dev, struct kobject *glue_dir)
 {
 	/* see if we live in a "glue" directory */
-	if (!live_in_glue_dir(glue_dir, dev))
+	if (!glue_dir || !dev->class ||
+	    glue_dir->kset != &dev->class->p->glue_dirs)
 		return;
 
 	mutex_lock(&gdp_mutex);
 	kobject_put(glue_dir);
 	mutex_unlock(&gdp_mutex);
+}
+
+static void cleanup_device_parent(struct device *dev)
+{
+	cleanup_glue_dir(dev, dev->kobj.parent);
 }
 
 static int device_add_class_symlinks(struct device *dev)
@@ -1019,7 +1006,6 @@ int device_add(struct device *dev)
 	struct device *parent = NULL;
 	struct kobject *kobj;
 	struct class_interface *class_intf;
-	struct kobject *glue_dir = NULL;
 	int error = -EINVAL;
 
 	dev = get_device(dev);
@@ -1065,10 +1051,8 @@ int device_add(struct device *dev)
 	/* first, register with generic layer. */
 	/* we require the name to be set before, and pass NULL */
 	error = kobject_add(&dev->kobj, dev->kobj.parent, NULL);
-	if (error) {
-		glue_dir = get_glue_dir(dev);
+	if (error)
 		goto Error;
-	}
 
 	/* notify platform of device entry */
 	if (platform_notify)
@@ -1157,10 +1141,9 @@ done:
 	device_remove_file(dev, &uevent_attr);
  attrError:
 	kobject_uevent(&dev->kobj, KOBJ_REMOVE);
-	glue_dir = get_glue_dir(dev);
 	kobject_del(&dev->kobj);
  Error:
-	cleanup_glue_dir(dev, glue_dir);
+	cleanup_device_parent(dev);
 	if (parent)
 		put_device(parent);
 name_error:
@@ -1233,7 +1216,6 @@ void put_device(struct device *dev)
 void device_del(struct device *dev)
 {
 	struct device *parent = dev->parent;
-	struct kobject *glue_dir = NULL;
 	struct class_interface *class_intf;
 
 	/* Notify clients of device removal.  This call must come
@@ -1275,9 +1257,8 @@ void device_del(struct device *dev)
 	if (platform_notify_remove)
 		platform_notify_remove(dev);
 	kobject_uevent(&dev->kobj, KOBJ_REMOVE);
-	glue_dir = get_glue_dir(dev);
+	cleanup_device_parent(dev);
 	kobject_del(&dev->kobj);
-	cleanup_glue_dir(dev, glue_dir);
 	put_device(parent);
 }
 
