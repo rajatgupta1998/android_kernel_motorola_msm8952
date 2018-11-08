6cb482d3c699b6b5798545f17a54355003c41226
Rohit Vashisht <rohitv88cf@gmail.com>
2018-11-08 19:36:24 +0530

    athene: Revert some 3.18 backports from motorola
    Revert "soc: qcom: pil: Avoid possible buffer overflow during Modem boot"
    This reverts commit d7b2ebf5dc64c82501f9a1bc365d90449cde4077.
    
    Revert "msm: ipa: Fix for missing int overflow check in the refcount library"
    This reverts commit caed711d817ed0c0e7a51351107207b0f25dd4e6.
    
    Revert "msm: mdss: Fix invalid dma attachment during fb shutdown"
    This reverts commit 07e4e07baa26006ec12adcbf61dbb687a834939a.
    
    Signed-off-by: Rohit Vashisht <rohitv88cf@gmail.com>

--- drivers/soc/qcom/pil-msa.c
+++ drivers/soc/qcom/pil-msa.c
@@ -562,16 +562,7 @@ int pil_mss_reset_load_mba(struct pil_desc *pil)
 		goto err_mba_data;
 	}
 	count = fw->size;
-
-	if (count <= SZ_1M) {
-		/* Ensures memcpy is done for max 1MB fw size */
-		memcpy(mba_virt, data, count);
-	} else {
-		dev_err(pil->dev, "%s fw image loading into memory is failed due to fw size overflow\n",
-			__func__);
-		ret = -EINVAL;
-		goto err_mss_reset;
-	}
+	memcpy(mba_virt, data, count);
 	wmb();
 
 	/* Load modem debug policy */
