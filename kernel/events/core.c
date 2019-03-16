8cab06b9fc105afc014e769fe1b93d183a74d15d
Rohit Vashisht <rohitv88cf@gmail.com>
2019-03-16 19:31:41 +0530

    athene: initial code cleanup
    Signed-off-by: Rohit Vashisht <rohitv88cf@gmail.com>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -3768,15 +3768,15 @@ static long _perf_ioctl(struct perf_event *event, unsigned int cmd,
 
 static long perf_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
-        struct perf_event *event = file->private_data;
-        struct perf_event_context *ctx;
-        long ret;
+	struct perf_event *event = file->private_data;
+	struct perf_event_context *ctx;
+	long ret;
 
-        ctx = perf_event_ctx_lock(event);
-        ret = _perf_ioctl(event, cmd, arg);
-        perf_event_ctx_unlock(event, ctx);
+	ctx = perf_event_ctx_lock(event);
+	ret = _perf_ioctl(event, cmd, arg);
+	perf_event_ctx_unlock(event, ctx);
 
-        return ret;
+	return ret;
 }
 
 #ifdef CONFIG_COMPAT
